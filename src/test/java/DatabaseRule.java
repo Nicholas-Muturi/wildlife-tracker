c77c8e350fbc3de33c8f49b45cd8c6a4941d6e45
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-20 21:34:21 +0300

    [chore]-Created classes for Animal, Ranger and Sighting. Also connected the project to the database

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,22 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource {
+
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test","nick","00000000");
+    }
+
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalQuery = "DELETE FROM animals *;";
+            String deleteSightingQuery = "DELETE FROM sightings *;";
+            String deleteRangerQuery = "DELETE FROM rangers *;";
+            con.createQuery(deleteAnimalQuery).executeUpdate();
+            con.createQuery(deleteSightingQuery).executeUpdate();
+            con.createQuery(deleteRangerQuery).executeUpdate();
+        }
+    }
+
+}
