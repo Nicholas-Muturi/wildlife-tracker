62463f67a0f66a618f5d5aa7a125d5ab7cef45f3
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-22 00:29:32 +0300

    [heroku] Added Procfile and port configuration in App.java

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/wildlife-tracker/bin/wildlife-tracker
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,6 +11,16 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        ProcessBuilder process = new ProcessBuilder();
+        int port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+
 
         //get: Homepage
         get("/",(request, response) -> {
