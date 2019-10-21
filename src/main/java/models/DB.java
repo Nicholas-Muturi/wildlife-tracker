3698f8d70d5f8a02d98621d88c9c4e01e1373d37
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-22 00:50:43 +0300

    [heroku] Configured postgresql for heroku deployment

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,8 +1,33 @@
 package models;
-
-import org.sql2o.Sql2o;
+import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","nick","00000000");
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
 
+        try{
+            if(System.getenv("DATABASE_URL") == null){
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+            }
+            else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? Credentials.username : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? Credentials.password : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e){
+            logger.error("unable to connect to database");
+        }
+
+    }
 }
